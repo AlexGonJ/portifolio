393ae329ac1803ee9b8ba2b8f34addcc876e431f
AlexGonJ <alexgoncalvesdtna@gmail.com>
2026-07-29 12:17:31 -0300

    feat pages event on meta view

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useState, useEffect } from 'react'
+import Script from 'next/script'
 import Navbar from '@/components/Navbar'
 import Hero from '@/components/Hero'
 import SelectedWork from '@/components/SelectedWork'
@@ -41,6 +42,29 @@ export default function Home() {
 
   return (
     <>
+      {/* 
+        ========================================================================
+        CÓDIGO DO PIXEL DA META (FACEBOOK) - HOMEPAGE
+        ========================================================================
+      */}
+      <Script id="meta-pixel-home" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          
+          fbq('init', '921564783637852'); 
+          fbq('track', 'PageView');
+          fbq('trackCustom', 'HomeView'); // Evento para visitas na Homepage
+        `}
+      </Script>
+      {/* ======================================================================== */}
+
       {isLoading && <Loader onComplete={() => setIsLoading(false)} />}
 
       <div className="smooth-wrapper" style={{ opacity: isLoading ? 0 : 1 }}>
--- app/servicos/marketing-imagem/page.tsx
+++ app/servicos/marketing-imagem/page.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from 'react'
 import Link from 'next/link'
+import Script from 'next/script'
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import Navbar from '@/components/Navbar'
@@ -434,6 +435,23 @@ export default function MarketingImagemPage() {
 
   return (
     <>
+      <Script id="meta-pixel-marketing" strategy="afterInteractive">
+        {\`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          
+          fbq('init', '921564783637852'); 
+          fbq('track', 'PageView');
+          fbq('trackCustom', 'ViewMarketingImagem');
+        \`}
+      </Script>
+
       {isLoading && <Loader onComplete={() => setIsLoading(false)} />}
 
       <div className="smooth-wrapper" style={{ opacity: isLoading ? 0 : 1 }}>
